ee414218c721fd18ef828df29165384edb48ae9b
diegonovoa@me.com <diegonovoa@me.com>
2018-07-01 16:55:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (name === "Susan") {
     console.log("Expected the name to be Susan")
 }
 
-if (height === "Susan") {
+if (height === "70") {
     console.log("The height is correct")
 } else {
     console.log("Expected height to be 70")
