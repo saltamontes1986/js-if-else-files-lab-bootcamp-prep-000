04f5eefe29810627490070700b4c3c081678d6f0
diegonovoa@me.com <diegonovoa@me.com>
2018-07-01 16:50:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-var name = "Joe"
-var height = "74"
-
-
-// Don't worry about this
-module.exports = { name, height
+var name = "Susan"
+var height = "70"
+if (index.name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected: Susan, Received: "+index.name)
 }
 
+
+
+if (index.height === "70") {
+    console.log("The height is correct")
+} else {
+    console.log("Expected: 70, Received: "+index.height)
+}
+// Don't worry about this
+module.exports = { name,
+height
+}
