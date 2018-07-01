a6eacec36587f8c09e0ed5fed96439d31ca55692
diegonovoa@me.com <diegonovoa@me.com>
2018-07-01 16:54:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ if (name === "Susan") {
     console.log("Expected the name to be Susan")
 }
 
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan")
+}
 // Don't worry about this
 module.exports = { name,
 height
